4db296a1625d90fa69aed8009808f4f360f683e7
Raden616Rizki <rizkiraden616@gmail.com>
2023-01-09 19:01:51 +0700

    freeze dotenv and other requirements

--- app.py
+++ app.py
@@ -1,28 +1,71 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://raden_616_rizki:LCzPylmLh3N7KP9j@cluster0.gcv3hdx.mongodb.net/?retryWrites=true&w=majority')
-db = client.dbsparta
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME =  os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+
+db = client[DB_NAME]
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+   return render_template('index.html')
 
 @app.route("/homework", methods=["POST"])
 def homework_post():
-    name_recieve = request.form['name_give']
-    comment_recieve = request.form['comment_give']
+    # sample_receive = request.form['sample_give']
+    name_receive = request.form['name_give']
+    comment_receive = request.form['comment_give']
     doc = {
-        'name': name_recieve,
-        'comment': comment_recieve
+        'name': name_receive,
+        'comment': comment_receive,
     }
-    db.fanmessage.insert_one(doc)
-    return jsonify({'msg':'POST request!'})
+    db.fanmessages.insert_one(doc)
+    return jsonify({'msg':'Comment Posted!'})
 
 @app.route("/homework", methods=["GET"])
 def homework_get():
-    message_list = list(db.fanmessage.find({}, {'_id': False}))
+    message_list = list(db.fanmessages.find({}, {'_id': False}))
     return jsonify({'messages': message_list})
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+   app.run('0.0.0.0', port=5000, debug=True)
+
+# from flask import Flask, render_template, request, jsonify
+# from pymongo import MongoClient
+# client = MongoClient('mongodb+srv://raden_616_rizki:LCzPylmLh3N7KP9j@cluster0.gcv3hdx.mongodb.net/?retryWrites=true&w=majority')
+# db = client.fanproject
+# app = Flask(__name__)
+
+# @app.route('/')
+# def home():
+#     return render_template('index.html')
+
+# @app.route("/homework", methods=["POST"])
+# def homework_post():
+#     name_recieve = request.form['name_give']
+#     comment_recieve = request.form['comment_give']
+#     doc = {
+#         'name': name_recieve,
+#         'comment': comment_recieve
+#     }
+#     db.fanmessage.insert_one(doc)
+#     return jsonify({'msg':'POST request!'})
+
+# @app.route("/homework", methods=["GET"])
+# def homework_get():
+#     message_list = list(db.fanmessage.find({}, {'_id': False}))
+#     return jsonify({'messages': message_list})
+
+# if __name__ == '__main__':
+#     app.run('0.0.0.0', port=5000, debug=True)
